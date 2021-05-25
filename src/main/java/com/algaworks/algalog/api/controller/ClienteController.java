8525a92792b0bbe1c1109898753f71aaeeb54ad4
JoseVinicius7 <jose.vinicius7@hotmail.com>
2021-05-24 21:58:51 -0300

    criação post

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -7,10 +7,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.api.models.Cliente;
@@ -40,4 +44,13 @@ public class ClienteController {
 				.orElse(ResponseEntity.notFound().build());
 	
 	}
+	
+	@PostMapping("/{id}")
+	@ResponseStatus (HttpStatus.CREATED)
+	public Cliente cadastrar(@RequestBody @PathVariable Cliente cliente){
+		return clienteRepository.save(cliente);
+	}
+	
+	
+	
 }
