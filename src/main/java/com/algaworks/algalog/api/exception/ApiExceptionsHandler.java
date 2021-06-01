00ef1f3c0d4af2d67cea66ec2a4a13544cf2e183
JoseVinicius7 <jose.vinicius7@hotmail.com>
2021-06-01 15:29:53 -0300

    Tratamento da BusinessExpception

--- src/main/java/com/algaworks/algalog/api/exception/ApiExceptionsHandler.java
+++ src/main/java/com/algaworks/algalog/api/exception/ApiExceptionsHandler.java
@@ -3,7 +3,6 @@ package com.algaworks.algalog.api.exception;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -14,6 +13,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -22,23 +22,21 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @ControllerAdvice
 public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
-	
-	
-	private MessageSource messageSource ;
-	
-	
+
+	private MessageSource messageSource;
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		List<ApiError.Campos> campos= new ArrayList<>();
+		List<ApiError.Campos> campos = new ArrayList<>();
 
-		for( ObjectError error : ex.getBindingResult().getAllErrors()) {
-			String nome = ((FieldError)error).getField();
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError) error).getField();
 			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
-			
+
 			campos.add(new ApiError.Campos(nome, mensagem));
 		}
-		
+
 		ApiError apiError = new ApiError();
 
 		apiError.setStatus(status.value());
@@ -46,6 +44,21 @@ public class ApiExceptionsHandler extends ResponseEntityExceptionHandler {
 		apiError.setCampos(campos);
 
 		return super.handleExceptionInternal(ex, apiError, headers, status, request);
+	}
+	
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<Object> handleBusiness(BusinessException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		ApiError apiError = new ApiError();
+
+		apiError.setStatus(status.value());
+		apiError.setData(LocalDateTime.now());
+		apiError.setTitulo(ex.getMessage());
+
+		
+		return handleExceptionInternal(ex,apiError, new HttpHeaders(), status, request);
+		
 	}
 
 }
